f0ef84ca5c660281e93cd68a0df3e60e96e0a453
deepanshu36 <deepanshu36>
2024-10-14 22:24:35 +0000

    laptimeadd

--- (Clone) deltalake/importfunctions/mergecond.py
+++ (Clone) deltalake/importfunctions/mergecond.py
@@ -2,7 +2,8 @@
 circuitsKey="dt.circuitId=st.circuitId"
 driversKey="dt.driverId=st.driverId"
 raceKey="dt.raceId=st.raceId"    
-resultsKey="dt.driverId=st.driverId AND dt.raceId=st.raceId and dt.resultId=st.resultId"     
+resultsKey="dt.driverId=st.driverId AND dt.raceId=st.raceId and dt.resultId=st.resultId"
+laptimesKey="dt.raceId=st.raceId AND dt.driverId=st.driverId and dt.stop=st.stop"     
 
 
 
