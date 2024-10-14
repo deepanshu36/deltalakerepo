94cfee3abd2100003dff7e0f629d47c87289aef7
deepanshu36 <deepanshu36>
2024-10-14 07:01:54 +0000

    parquet

---
+++ (Clone) deltalake/raw/readparquet.py
@@ -0,0 +1,53 @@
+# Databricks notebook source
+# MAGIC
+# MAGIC %run /Workspace/Users/deepanshuk36@gmail.com/deltalake/importfunctions/impfun
+# MAGIC
+
+# COMMAND ----------
+
+dbutils.fs.rm('/mnt/bronzeadls/results/',True)
+
+# COMMAND ----------
+
+def getlatest(location):
+    a=[]
+    
+    for path in dbutils.fs.ls(location):
+        a.append((path[0],path[0].split('/')[-1][8:].split('.')[0].replace('_','-')))
+    a.sort()
+    if(len(a))==0:
+        return None
+
+    return a[-1]
+
+
+filename=getlatest('/mnt/sourceeadlsgen2/results')[0]
+filedate=getlatest('/mnt/sourceeadlsgen2/results')[1]
+
+print(getlatest('/mnt/sourceeadlsgen2/results/')[0],filedate)
+
+df=spark.read.format("parquet").option('header',True).option('inferschema',True).load(filename)
+
+# display(df)
+
+from pyspark.sql.types import *
+
+# checkschema(df.dtypes,spark.read.table('bronze.results').dtypes[0:-3])
+
+from pyspark.sql.functions import input_file_name,split,current_timestamp
+
+df=addload_timestamp(df,None)
+df=df.withColumn("file_date",lit(filedate))
+
+# print(df.dtypes)
+display(df)
+
+df=df.dropDuplicates()
+
+if(filedate is not None and filedate in getlatest('/mnt/bronzeadls/results/')[0]):
+    print(filedate,df["file_date"])
+    df.write.partitionBy('file_date').mode('overwrite')\
+    .option('replaceWhere', f"file_date=='{filedate}'")\
+    .save('/mnt/bronzeadls/results/')
+else:
+    df.write.partitionBy('file_date').mode('overwrite').save('/mnt/bronzeadls/results/')
