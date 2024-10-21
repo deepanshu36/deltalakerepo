c5d64bbe6d40e9c5b4fa1d50fbb8bbabbff40751
deepanshu36 <deepanshu36>
2024-10-21 19:45:51 +0000

    masterchange

--- (Clone) deltalake/vaccum/mastervaccum.py
+++ (Clone) deltalake/vaccum/mastervaccum.py
@@ -1,6 +1,5 @@
 # Databricks notebook source
 # parameteries
-%run /Workspace/Repos/deepanshuk36@gmail.com/deltalakerepo/(Clone) deltalake/vaccum/bronzevaccum
 
 
 # COMMAND ----------
@@ -11,11 +10,11 @@ databasename=dbutils.widgets.get("databasename")
 
 
 if(databasename in 'bronze'):
-    %run /Workspace/Repos/deepanshuk36@gmail.com/deltalakerepo/(Clone) deltalake/vaccum/bronzevaccum
+    %run "/Workspace/Repos/deepanshuk36@gmail.com/deltalakerepo/(Clone) deltalake/vaccum/bronzevaccum"
 elif(databasename in 'silver'):
-    %run /Workspace/Repos/deepanshuk36@gmail.com/deltalakerepo/(Clone) deltalake/vaccum/silvervaccum
+    %run "/Workspace/Repos/deepanshuk36@gmail.com/deltalakerepo/(Clone) deltalake/vaccum/silvervaccum"
 elif (databasename in 'gold'):
-    %run /Workspace/Repos/deepanshuk36@gmail.com/deltalakerepo/(Clone) deltalake/vaccum/goldvaccum
+    %run "/Workspace/Repos/deepanshuk36@gmail.com/deltalakerepo/(Clone) deltalake/vaccum/goldvaccum"
 else:
     raise Exception("invalid databse name")    
     
